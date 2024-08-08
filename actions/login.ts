21c0ba35dc632a78151593169f9bafa9973266fd
Nabil <nabil.ziani@brightest.be>
2024-08-08 22:00:31 +0200

    google oauth

--- actions/login.ts
+++ actions/login.ts
@@ -2,8 +2,11 @@
 
 import * as z from 'zod'
 
+import { headers } from "next/headers";
 import { LoginSchema } from '@/schemas';
 import { createClient } from "@/utils/supabase/server";
+import { Provider } from '@supabase/supabase-js';
+import { redirect } from 'next/navigation';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values)
@@ -25,4 +28,24 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     }
 
     return { success: 'You will be redirected shortly!' }
+};
+
+export const loginWithOAuth = async (provider: Provider) => {
+    const origin = headers().get("origin");
+
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider,
+        options: {
+            redirectTo: `${origin}/auth/callback`,
+        },
+    })
+
+    if (error) {
+        return { error: error.message }
+    }
+
+    if (data.url) {
+        redirect(data.url) // use the redirect API for your server framework
+    }
 };
--- components/oauth.tsx
+++ components/oauth.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { loginWithOAuth } from '@/actions/login';
 import { type Provider } from '@supabase/supabase-js';
+import Image from 'next/image';
 
 type OAuthProviders = {
     name: Provider;
@@ -21,31 +25,19 @@ export default function OauthSignIn() {
         }
     ];
 
-    // const [isSubmitting, setIsSubmitting] = useState(false);
-
-    // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    //     setIsSubmitting(true); // Disable the button while the request is being handled
-    //     await signInWithOAuth(e);
-    //     setIsSubmitting(false);
-    // };
+    const onClick = (provider: Provider) => {
+        loginWithOAuth(provider)
+    }
 
     return (
         <div className="flex flex-row justify-evenly">
             {oAuthProviders.map((provider) => (
-                <form key={provider.name}>
-                    <input type="hidden" name="provider" value={provider.name} />
-
-                    <button
-                        type="submit"
-                        className="aspect-square w-10 h-10 rounded-full bg-white flex items-center justify-center transition-colors duration-300 dark:bg-zinc-100 dark:hover:bg-zinc-600"
-                    >
-                        <img
-                            src={provider.icon}
-                            alt="oauth logo"
-                            className="h-6 w-6"
-                        />
-                    </button>
-                </form>
+                <button
+                    onClick={() => onClick(provider.name)}
+                    className="aspect-square w-10 h-10 rounded-full bg-white flex items-center justify-center transition-colors duration-300 dark:bg-zinc-100 dark:hover:bg-zinc-600"
+                >
+                    <Image width={30} height={30} src={provider.icon} alt="oauth logo" />
+                </button>
             ))}
         </div>
     );
