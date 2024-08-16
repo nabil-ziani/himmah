4fdcab417cbc55227e7ea0417db40828ce0aa660
Nabil <nabil.ziani@brightest.be>
2024-08-16 19:27:17 +0200

    tools section done

--- components/sections/features.tsx
+++ components/sections/features.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 const Features = () => {
     return (
         <>
-            <section id='about' className="flex w-full h-[100vh] flex-col items-center justify-center">
+            <section id='features' className="flex w-full h-[100vh] flex-col items-center justify-center">
                 <motion.h2
                     initial={{ y: 48, opacity: 0 }}
                     whileInView={{ y: 0, opacity: 1 }}
@@ -20,7 +20,7 @@ const Features = () => {
                     whileInView={{ y: 0, opacity: 1 }}
                     transition={{ ease: 'easeInOut', duration: 0.75 }}
                     // viewport={{ once: true }}
-                    className='mx-auto max-w-6xl text-center px-8'>
+                    className='mx-auto max-w-7xl text-center px-8'>
                     <p className='text-xl leading-relaxed mb-20'>
                         We intend to be a complete platform with tools for focus, planning, relaxing, communicating and much more.
                         Everything necessary to be productive will be gathered on one platform in the most optimal way for you.
@@ -29,14 +29,19 @@ const Features = () => {
 
                     <ul
                         role="list"
-                        className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-20  sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-5"
+                        className="mx-auto mt-10 grid max-w-3xl grid-cols-1 gap-x-20  sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-5"
                     >
                         {focusFeatures.map((feature) => (
                             <li key={feature.title} onClick={() => {
                                 // setContent({ title: goal.title, description: goal.description, image: goal.image })
                                 // setIsOpen(true)
                             }}>
-                                <motion.img alt="" src={feature.image} className="mx-auto h-40 w-40 object-cover rounded-lg hover:cursor-pointer" whileHover={{ scale: 1.2 }} />
+                                <motion.img
+                                    alt=""
+                                    src={feature.image}
+                                    className="mx-auto h-40 w-40 object-cover rounded-lg hover:cursor-pointer"
+                                    whileHover={{ scale: 1.1 }}
+                                    whileTap={{ scale: 0.9 }} />
                                 <h3 className="mt-6 text-lg text-center font-semibold leading-7 tracking-tight text-gray-900">
                                     {feature.title}
                                 </h3>
