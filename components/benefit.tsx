ae21a1a6d8346d9b76fc807bdf88bc91e138196d
Nabil <nabil.ziani@brightest.be>
2024-08-18 21:20:04 +0200

    making text bold

--- components/benefit.tsx
+++ components/benefit.tsx
@@ -1,15 +1,16 @@
 'use client'
 
 import { motion } from 'framer-motion'
+import { ReactNode } from 'react'
 
 interface BenefitProps {
     alignment: string
-    text: string
     icon: string
     color: string
+    children: ReactNode
 }
 
-const Benefit = ({ alignment, text, icon, color }: BenefitProps) => {
+const Benefit = ({ alignment, icon, color, children }: BenefitProps) => {
     return (
         <motion.div
             initial={{ y: 48, opacity: 0 }}
@@ -18,10 +19,8 @@ const Benefit = ({ alignment, text, icon, color }: BenefitProps) => {
             // viewport={{ once: true }}
             className={`flex flex-col ${alignment == 'left' ? 'items-start' : 'items-end'} w-[70%]`}>
             <div className='flex flex-col justify-center items-center'>
-                <p className='text-2xl text-center leading-relaxed w-[600px] max-w-[90%]'>
-                    {text}
-                </p>
-                <div className={`rounded-full w-52 h-52 flex justify-center mt-10 items-center bg-gray-200`}>
+                {children}
+                <div className={`rounded-full w-52 h-52 flex justify-center mt-10 items-center ${color}`}>
                     <img alt="" src={icon} className="mx-auto h-20 w-20 rounded-full hover:cursor-pointer" />
                 </div>
             </div >
--- components/sections/benefits.tsx
+++ components/sections/benefits.tsx
@@ -16,13 +16,41 @@ const Benefits = () => {
                     Why Choose Us
                 </motion.h2>
 
-                <Benefit color={'gray'} alignment='left' text='Himmah is currently free to use!' icon='/benefits/free.png' />
-                <Benefit color={'gray'} alignment='right' text='Himmah increases motivation and focus, allowing you to become more productive!' icon='/benefits/plant.png' />
-                <Benefit color={'gray'} alignment='left' text='Himmah gathers all you need on one platform. No more need to open multiple apps and tabs!' icon='/benefits/tabs.png' />
-                <Benefit color={'gray'} alignment='right' text='Himmah is not only useful, but also attractive and easy-to-use!' icon='/benefits/attractive_design.png' />
-                <Benefit color={'gray'} alignment='left' text='Himmah is a very young platform, allowing you to decide its future via feedback!' icon='/benefits/watering_plant.png' />
-                <Benefit color={'gray'} alignment='right' text='Himmah has owners with high aspirations. Lend us your support and we will amaze you! (in shaa Allah)' icon='/benefits/rocket.png' />
-                <Benefit color={'gray'} alignment='left' text='Lastly, we do NOT share any information with third parties as we value your privacy!' icon='/benefits/privacy.png' />
+                <Benefit color={'bg-red-50'} alignment='left' icon='/benefits/free.png'>
+                    <p className='text-2xl text-center leading-relaxed w-[600px]'>
+                        Himmah is currently <span className='font-bold'>free to use!</span>
+                    </p>
+                </Benefit>
+                <Benefit color={'bg-green-50'} alignment='right' icon='/benefits/plant.png'>
+                    <p className='text-2xl text-center leading-relaxed w-[600px]'>
+                        Himmah increases motivation and focus, allowing you to become more <span className='font-bold'>productive!</span>
+                    </p>
+                </Benefit>
+                <Benefit color={'bg-slate-200'} alignment='left' icon='/benefits/tabs.png'>
+                    <p className='text-2xl text-center leading-relaxed w-[600px]'>
+                        Himmah gathers all you need on <span className='font-bold'>one platform.</span> No more need to open multiple apps and tabs!
+                    </p>
+                </Benefit>
+                <Benefit color={'bg-orange-50'} alignment='right' icon='/benefits/attractive_design.png'>
+                    <p className='text-2xl text-center leading-relaxed w-[600px]'>
+                        Himmah is not only useful, but also <span className='font-bold'>attractive</span> and <span className='font-bold'>easy-to-use!</span>
+                    </p>
+                </Benefit>
+                <Benefit color={'bg-gray-50'} alignment='left' icon='/benefits/watering_plant.png'>
+                    <p className='text-2xl text-center leading-relaxed w-[600px]'>
+                        Himmah is a very young platform, allowing you to decide its future via <span className='font-bold'>feedback!</span>
+                    </p>
+                </Benefit>
+                <Benefit color={'bg-purple-50'} alignment='right' icon='/benefits/rocket.png'>
+                    <p className='text-2xl text-center leading-relaxed w-[600px]'>
+                        Himmah has owners with <span className='font-bold'>high aspirations.</span> Lend us your support and we will amaze you! <span className='text-gray-600'>(in shaa Allah)</span>
+                    </p>
+                </Benefit>
+                <Benefit color={'gray'} alignment='left' icon='/benefits/privacy.png'>
+                    <p className='text-2xl text-center leading-relaxed w-[600px]'>
+                        Lastly, we do NOT share any information with third parties as we <span className='font-bold'>value your privacy!</span>
+                    </p>
+                </Benefit>
             </section>
         </>
     )
