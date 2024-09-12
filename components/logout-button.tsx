e9266cabcfdb86f465c11f35884a49d347f15c74
Nabil <nabil.ziani@brightest.be>
2024-09-12 13:14:27 +0200

    add logout to sidebar and add avatar in nav

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -2,6 +2,7 @@
 
 import { logout } from '@/actions/logout'
 import { Button } from './ui/button'
+import { LogOutIcon } from 'lucide-react'
 
 const LogoutButton = () => {
 
@@ -10,8 +11,11 @@ const LogoutButton = () => {
     }
 
     return (
-        <Button variant={'destructive'} onClick={onClick}>
-            Logout
+        <Button className='flex gap-4 items-center p-6 mb-2 rounded-lg justify-start' variant={'destructive'} onClick={onClick}>
+            <LogOutIcon />
+            <p className='text-md font-semibold max-lg:hidden'>
+                Logout
+            </p>
         </Button>
     )
 }
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,6 +6,7 @@ import { usePathname } from 'next/navigation'
 
 import Link from 'next/link';
 import { LayoutDashboard, LucideFocus, Settings2Icon, UsersRoundIcon } from 'lucide-react';
+import LogoutButton from './logout-button';
 
 const Sidebar = ({ }) => {
     const pathname = usePathname();
@@ -28,18 +29,22 @@ const Sidebar = ({ }) => {
 
     return (
         <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-[#303030] p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
-            <div className="flex flex-1 flex-col gap-6">
-                {sidebarLinks.map((link) => {
-                    const isActive = pathname === link.route;
-                    return (
-                        <Link href={`${link.route}`} key={link.label} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start text-[#303030] hover:bg-white/15', { 'bg-white hover:bg-white': isActive })}>
-                            {renderIcon(link.route, isActive ? '#303030' : 'white')}
-                            <p className={`text-md font-semibold max-lg:hidden ${isActive ? 'text-[#303030]' : 'text-white'}`}>
-                                {link.label}
-                            </p>
-                        </Link>
-                    )
-                })}
+            <div className="flex flex-1 gap-6 justify-between flex-col">
+                <div className='flex flex-col gap-6'>
+                    {sidebarLinks.map((link) => {
+                        const isActive = pathname === link.route;
+                        return (
+                            <Link href={`${link.route}`} key={link.label} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start text-[#303030] hover:bg-white/15', { 'bg-white hover:bg-white': isActive })}>
+                                {renderIcon(link.route, isActive ? '#303030' : 'white')}
+                                <p className={`text-md font-semibold max-lg:hidden ${isActive ? 'text-[#303030]' : 'text-white'}`}>
+                                    {link.label}
+                                </p>
+                            </Link>
+                        )
+                    })}
+                </div>
+
+                <LogoutButton />
             </div>
         </section>
     )
