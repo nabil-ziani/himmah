2949c777bd94bed4d9fb3e0fdbd8c93b4818ec2a
Nabil <nabil_ee@hotmail.com>
2024-10-02 19:54:59 +0200

    fix font landing page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,23 +1,23 @@
-import { ThemeProvider } from "@/components/theme-provider";
-import "../globals.css";
-import { poppins } from "../ui/fonts";
+import { ThemeProvider } from "@/components/theme-provider"
+import "../globals.css"
+import { poppins } from "../ui/fonts"
 
-const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
+const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Himmah",
-  description: "Productivity App"
+	metadataBase: new URL(defaultUrl),
+	title: "Himmah",
+	description: "Productivity App"
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" suppressHydrationWarning className={`${poppins.variable} antialiased !scroll-smooth`}>
-      <body>
-        <ThemeProvider attribute="class" forcedTheme="light" enableSystem={false}>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${poppins.className} antialiased`}>
+				<ThemeProvider attribute="class" forcedTheme="light" enableSystem={false}>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html >
+	)
 }
