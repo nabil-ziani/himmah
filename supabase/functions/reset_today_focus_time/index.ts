5d013b233df6387eac673fbdeae5962f68deb892
Nabil <nabil_ee@hotmail.com>
2024-10-07 11:09:15 +0200

    fix time cron job

--- supabase/functions/reset_today_focus_time/index.ts
+++ supabase/functions/reset_today_focus_time/index.ts
@@ -5,7 +5,6 @@ import { createClient, SupabaseClient } from 'jsr:@supabase/supabase-js@2'
 import 'https://deno.land/x/dotenv@v3.2.2/load.ts'
 
 Deno.serve(async (req) => {
-
     const supabase = createClient(
         Deno.env.get('SUPABASE_URL') ?? '',
         Deno.env.get('SUPABASE_ANON_KEY') ?? '',
@@ -14,7 +13,7 @@ Deno.serve(async (req) => {
     // Check of de request een POST-verzoek is
     if (req.method === "POST") {
         const { error } = await supabase
-            .from('public.profiles')
+            .from('profiles')
             .update({ today_focus_time: 0 })
             .neq('today_focus_time', 0)
 
